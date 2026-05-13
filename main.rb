1bdbd249b8b937c6efd8d20a0634896e483b27dd
Seb <everydayseb@pm.me>
2026-05-13 08:36:53 +0100

    Add letter hiding

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
+def word_with_known_letters(word, guessed_letters)
+  new_string = ''
+  word.each_char { |char| new_string << (guessed_letters.include?(char) ? char : '_') }
+  new_string
+end
+
 MIN_WORD_LENGTH = 5
 MAX_WORD_LENGTH = 12
 dictionary_file = 'data/google-10000-english-no-swears.txt'
@@ -5,4 +11,5 @@ dictionary = File.readlines(dictionary_file)
                  .map(&:chomp)
                  .select { |word| word.size.between?(MIN_WORD_LENGTH, MAX_WORD_LENGTH) }
 word = dictionary.sample
-p word
+guessed_letters = ['a']
+puts word_with_known_letters(word, guessed_letters)
